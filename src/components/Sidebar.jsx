1bb7e575856733a6c009b9871c8fe7ac7041b33a
dasboardhub-lab <dasboardhub@gmail.com>
2026-01-07 15:30:46 +0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,2 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-
-export default function Sidebar() {
-  return (
-    <aside style={{ width: 220, padding: 20 }}>
-      <h3>DompetKu</h3>
-      <nav>
-        <NavLink to="/">Dashboard</NavLink><br />
-        <NavLink to="/transaksi">Transaksi</NavLink><br />
-        <NavLink to="/laporan">Laporan</NavLink><br />
-        <NavLink to="/pengaturan">Pengaturan</NavLink>
-      </nav>
-    </aside>
-  );
-}
